d7e818ca79d04600cb77f15a37a1d0e712d20edb
JANFAN-L <43380308+JANFAN-L@users.noreply.github.com>
2020-03-21 12:28:46 -0400

    text edit screen

---
+++ src/components/edit_screen/EnterTextEdit.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react'
+import Nabar from './Navbar.js'
+import TextEditSidebar from './TextEditSidebar' 
+import TextEditWorkspace from './TextEditWorkspace'
+
+export class EnterTextEdit extends Component{
+
+}
